d38d7f937c012f3f81ff6e71e76b34cb1c1346a3
Tomás F. Vilalta <84082278+TomasVilalta@users.noreply.github.com>
2023-12-01 15:34:46 -0300

    aaa

--- public/hotjarInit.js
+++ public/hotjarInit.js
@@ -1,6 +1,13 @@
-import Hotjar from "@hotjar/browser";
-
-const siteId = 3768294;
-const hotjarVersion = 6;
-
-Hotjar.init(siteId, hotjarVersion);
+(function (h, o, t, j, a, r) {
+  h.hj =
+    h.hj ||
+    function () {
+      (h.hj.q = h.hj.q || []).push(arguments);
+    };
+  h._hjSettings = { hjid: 3768294, hjsv: 6 };
+  a = o.getElementsByTagName("head")[0];
+  r = o.createElement("script");
+  r.async = 1;
+  r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
